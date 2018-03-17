6a5d1646f009cd91fba034657a54fd565fa64c55
David Eder <david.eder@me.com>
2018-03-17 16:12:35 +0100

    xml file, Downloader class

---
+++ src/main/java/Downloader.java
@@ -0,0 +1,16 @@
+import org.apache.commons.io.FileUtils;
+
+import static javafx.scene.input.DataFormat.URL;
+
+/**
+ * Created by davideder on 17.03.18.
+ */
+
+
+public class Downloader {
+
+    public void download(String url){
+        FileUtils.copyURLToFile(URL, File);
+
+    }
+}
