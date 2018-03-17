916a665a9b589412bd34c2c0da851b9fe344c84a
David Eder <david.eder@me.com>
2018-03-17 16:37:25 +0100

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Hello!");
+
+            Downloader downloader = new Downloader();
+            downloader.download("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:AUFZUGOGD&srsName=EPSG:4326&outputFormat=csv", "AUFZUGOGD.csv");
+            downloader.download("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UBAHNHALTOGD&srsName=EPSG:4326&outputFormat=csv", "UBAHNHALTOGD.csv");
+
         }
 }
